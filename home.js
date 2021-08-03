4edd3f3beb22ef51ace88c03933bbb060604d743
akshaya-pro <88369889+akshaya-pro@users.noreply.github.com>
2021-08-03 13:01:04 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+const home = document.getElementById("home-button");
+home.onclick = () => {
+    location.href = "./home.html";
+};
+
+const bullying = document.getElementById("bullying-button");
+bullying.onclick = () => {
+    location.href = "./bullying.html";
+};
+
+const cyberbullying = document.getElementById("cyberbullying-button");
+cyberbullying.onclick = () => {
+    location.href = "./cyberbullying.html";
+};
+
+const prevention = document.getElementById("prevention-button");
+prevention.onclick = () => {
+    location.href = "./prevention.html";
+}
